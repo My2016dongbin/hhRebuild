920b1023aed4673f91a2d9fd8ffe31047d197707
dongbin <1349975620@qq.com>
2025-03-18 18:49:49 +0800

    大庆 -国标换rtsp

--- app/src/main/java/com/haohai/platform/fireforestplatform/ui/activity/TaskListInfoActivity.java
+++ app/src/main/java/com/haohai/platform/fireforestplatform/ui/activity/TaskListInfoActivity.java
@@ -33,6 +33,7 @@ import com.scwang.smartrefresh.layout.listener.SimpleMultiPurposeListener;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import android.widget.Toast;
 
 public class TaskListInfoActivity extends BaseLiveActivity<ActivityTaskListInfoBinding, TaskListInfoViewModel> {
 
@@ -121,126 +122,131 @@ public class TaskListInfoActivity extends BaseLiveActivity<ActivityTaskListInfoB
 
     @SuppressLint("SetTextI18n")
     private void updateUi(TaskList taskList) {
-        if(obtainViewModel().taskLists.getValue()!=null &&
-                Objects.requireNonNull(obtainViewModel().taskLists.getValue()).getRoomId()!=null){
-            binding.mapContentView.setVisibility(View.VISIBLE);
-        }else{
-            binding.mapContentView.setVisibility(View.GONE);
-        }
-        binding.lineName.title.setText("任务名称");
-        binding.lineName.content.setText(taskList.getTaskContent());
-        binding.lineType.title.setText("任务类型");
-        binding.lineType.content.setText((taskList.getTaskContent()+"").contains("卡口")?"卡口报警任务":parseTaskType(taskList.getTaskType()));
-        binding.lineTime.title.setText("开始时间");
-        binding.lineTime.content.setText(StringData.parse19(taskList.getTaskStartTime()));
-        binding.lineLngLat.title.setText("任务经纬度");
         try{
-            binding.lineLngLat.content.setText(taskList.getPosition().getLng() + "," + taskList.getPosition().getLat());
-        }catch (Exception e){
-            Log.e("TAG", "updateUi: " + e.getMessage() );
-        }
-        binding.lineAddress.title.setText("任务状态");
-        binding.lineAddress.content.setText(parseState(taskList.getStatus()));
-        binding.lineHandle.title.setText("火警类型");
-        binding.lineHandle.content.setText(parseFireType(taskList.getFireType()));
-        binding.lineHandleTime.title.setText("结束时间");
-        binding.lineHandleTime.content.setText(StringData.parse19(taskList.getTaskEndTime()));
-        binding.lineHandleUser.title.setText("发起人");
-        binding.lineHandleUser.content.setText(taskList.getCreateUser());
-        binding.lineHandleOptions.title.setText("任务描述");
-        binding.lineHandleOptions.content.setText(taskList.getDescription());
-        List<String> urlList = StringData.parseList(taskList.getTaskImg());
-        binding.llPictures.removeAllViews();
-        for (int i = 0; i < urlList.size(); i++) {
-            String urls = urlList.get(i);
-            View p_ = LayoutInflater.from(this).inflate(R.layout.image, null);
-            ImageView image = p_.findViewById(R.id.image);
-            Glide.with(this).load(urls).error(R.drawable.ic_no_pic).into(image);
-            image.setOnClickListener(v -> {
-                Intent intent;
-                if(urls!=null && urls.contains("mp4")){
-                    //视频
-                    intent = new Intent(TaskListInfoActivity.this, VideoStreamActivity.class);
-                }else{
-                    //图片
-                    intent = new Intent(this, PhotoViewerActivity.class);
-                }
-                intent.putExtra("url",urls);
-                startActivity(intent);
-            });
-            binding.llPictures.addView(p_);
-        }
-        binding.textStatus.setText(parseState(taskList.getStatus()));
-
-        List<TaskList.TaskDetailDTOList> taskDetailDTOList = taskList.getTaskDetailDTOList();
-        binding.llUpload.removeAllViews();
-        if(taskDetailDTOList==null || taskDetailDTOList.size()==0 ){
-            binding.titleUpload.setVisibility(View.GONE);
-        }
-        if(taskDetailDTOList!=null){
-            for (int i = 0; i < taskDetailDTOList.size(); i++) {
-                TaskList.TaskDetailDTOList model = taskDetailDTOList.get(i);
-                View view = LayoutInflater.from(this).inflate(R.layout.upload_info, null);
-                TextView user = view.findViewById(R.id.user);
-                TextView time = view.findViewById(R.id.time);
-                TextView live_detail = view.findViewById(R.id.live_detail);
-                TextView live_other = view.findViewById(R.id.live_other);
-                LinearLayout ll_pictures = view.findViewById(R.id.ll_pictures);
-                LinearLayout ll_videos = view.findViewById(R.id.ll_videos);
-                user.setText(model.getCreateUser());
-                time.setText(CommonUtil.parse19String(model.getCreateTime(),model.getCreateTime()));
-                live_detail.setText(model.getSiteConditions());
-                live_other.setText(model.getOtherConditions());
-                List<String> pictures = CommonUtil.parseListString(model.getImgUrl());
-                List<String> videos = CommonUtil.parseListString(model.getVideoUrl());
-                ll_pictures.removeAllViews();
-                for (int m = 0; m < pictures.size(); m++) {
-                    String s = pictures.get(m);
-                    View p_ = LayoutInflater.from(this).inflate(R.layout.image, null);
-                    ImageView image = p_.findViewById(R.id.image);
-                    Glide.with(this).load(s).error(R.drawable.ic_no_pic).into(image);
-                    image.setOnClickListener(v -> {
-                        Intent intent;
-                        if(s!=null && s.contains("mp4")){
-                            //视频
-                            intent = new Intent(TaskListInfoActivity.this, VideoStreamActivity.class);
-                        }else{
-                            //图片
-                            intent = new Intent(this, PhotoViewerActivity.class);
-                        }
-                        intent.putExtra("url",s);
-                        startActivity(intent);
-                    });
-                    ll_pictures.addView(p_);
-                }
-                ll_videos.removeAllViews();
-                for (int m = 0; m < videos.size(); m++) {
-                    String s = videos.get(m);
-                    View v_ = LayoutInflater.from(this).inflate(R.layout.image, null);
-                    ImageView image = v_.findViewById(R.id.image);
-                    Glide.with(this).load(s).error(R.drawable.ic_no_pic).into(image);
-                    image.setOnClickListener(v -> {
-                        Intent intent;
-                        if(s!=null && s.contains("mp4")){
-                            //视频
-                            intent = new Intent(TaskListInfoActivity.this, VideoStreamActivity.class);
-                        }else{
-                            //图片
-                            intent = new Intent(this, PhotoViewerActivity.class);
-                        }
-                        intent.putExtra("url",s);
-                        startActivity(intent);
-                    });
-                    ll_videos.addView(v_);
-                }
-
-                binding.llUpload.addView(view);
+            if(obtainViewModel().taskLists.getValue()!=null &&
+                    Objects.requireNonNull(obtainViewModel().taskLists.getValue()).getRoomId()!=null){
+                binding.mapContentView.setVisibility(View.VISIBLE);
+            }else{
+                binding.mapContentView.setVisibility(View.GONE);
             }
-        }
+            binding.lineName.title.setText("任务名称");
+            binding.lineName.content.setText(taskList.getTaskContent());
+            binding.lineType.title.setText("任务类型");
+            binding.lineType.content.setText((taskList.getTaskContent()+"").contains("卡口")?"卡口报警任务":parseTaskType(taskList.getTaskType()));
+            binding.lineTime.title.setText("开始时间");
+            binding.lineTime.content.setText(StringData.parse19(taskList.getTaskStartTime()));
+            binding.lineLngLat.title.setText("任务经纬度");
+            try{
+                binding.lineLngLat.content.setText(taskList.getPosition().getLng() + "," + taskList.getPosition().getLat());
+            }catch (Exception e){
+                Log.e("TAG", "updateUi: " + e.getMessage() );
+            }
+            binding.lineAddress.title.setText("任务状态");
+            binding.lineAddress.content.setText(parseState(taskList.getStatus()));
+            binding.lineHandle.title.setText("火警类型");
+            binding.lineHandle.content.setText(parseFireType(taskList.getFireType()));
+            binding.lineHandleTime.title.setText("结束时间");
+            binding.lineHandleTime.content.setText(StringData.parse19(taskList.getTaskEndTime()));
+            binding.lineHandleUser.title.setText("发起人");
+            binding.lineHandleUser.content.setText(taskList.getCreateUser());
+            binding.lineHandleOptions.title.setText("任务描述");
+            binding.lineHandleOptions.content.setText(taskList.getDescription());
+            List<String> urlList = StringData.parseList(taskList.getTaskImg());
+            binding.llPictures.removeAllViews();
+            for (int i = 0; i < urlList.size(); i++) {
+                String urls = urlList.get(i);
+                View p_ = LayoutInflater.from(this).inflate(R.layout.image, null);
+                ImageView image = p_.findViewById(R.id.image);
+                Glide.with(this).load(urls).error(R.drawable.ic_no_pic).into(image);
+                image.setOnClickListener(v -> {
+                    Intent intent;
+                    if(urls!=null && urls.contains("mp4")){
+                        //视频
+                        intent = new Intent(TaskListInfoActivity.this, VideoStreamActivity.class);
+                    }else{
+                        //图片
+                        intent = new Intent(this, PhotoViewerActivity.class);
+                    }
+                    intent.putExtra("url",urls);
+                    startActivity(intent);
+                });
+                binding.llPictures.addView(p_);
+            }
+            binding.textStatus.setText(parseState(taskList.getStatus()));
 
-        //消息列表进入-暂时隐藏
-        if(obtainViewModel().message){
-            binding.llButton.setVisibility(View.GONE);
+            List<TaskList.TaskDetailDTOList> taskDetailDTOList = taskList.getTaskDetailDTOList();
+            binding.llUpload.removeAllViews();
+            if(taskDetailDTOList==null || taskDetailDTOList.size()==0 ){
+                binding.titleUpload.setVisibility(View.GONE);
+            }
+            if(taskDetailDTOList!=null){
+                for (int i = 0; i < taskDetailDTOList.size(); i++) {
+                    TaskList.TaskDetailDTOList model = taskDetailDTOList.get(i);
+                    View view = LayoutInflater.from(this).inflate(R.layout.upload_info, null);
+                    TextView user = view.findViewById(R.id.user);
+                    TextView time = view.findViewById(R.id.time);
+                    TextView live_detail = view.findViewById(R.id.live_detail);
+                    TextView live_other = view.findViewById(R.id.live_other);
+                    LinearLayout ll_pictures = view.findViewById(R.id.ll_pictures);
+                    LinearLayout ll_videos = view.findViewById(R.id.ll_videos);
+                    user.setText(model.getCreateUser());
+                    time.setText(CommonUtil.parse19String(model.getCreateTime(),model.getCreateTime()));
+                    live_detail.setText(model.getSiteConditions());
+                    live_other.setText(model.getOtherConditions());
+                    List<String> pictures = CommonUtil.parseListString(model.getImgUrl());
+                    List<String> videos = CommonUtil.parseListString(model.getVideoUrl());
+                    ll_pictures.removeAllViews();
+                    for (int m = 0; m < pictures.size(); m++) {
+                        String s = pictures.get(m);
+                        View p_ = LayoutInflater.from(this).inflate(R.layout.image, null);
+                        ImageView image = p_.findViewById(R.id.image);
+                        Glide.with(this).load(s).error(R.drawable.ic_no_pic).into(image);
+                        image.setOnClickListener(v -> {
+                            Intent intent;
+                            if(s!=null && s.contains("mp4")){
+                                //视频
+                                intent = new Intent(TaskListInfoActivity.this, VideoStreamActivity.class);
+                            }else{
+                                //图片
+                                intent = new Intent(this, PhotoViewerActivity.class);
+                            }
+                            intent.putExtra("url",s);
+                            startActivity(intent);
+                        });
+                        ll_pictures.addView(p_);
+                    }
+                    ll_videos.removeAllViews();
+                    for (int m = 0; m < videos.size(); m++) {
+                        String s = videos.get(m);
+                        View v_ = LayoutInflater.from(this).inflate(R.layout.image, null);
+                        ImageView image = v_.findViewById(R.id.image);
+                        Glide.with(this).load(s).error(R.drawable.ic_no_pic).into(image);
+                        image.setOnClickListener(v -> {
+                            Intent intent;
+                            if(s!=null && s.contains("mp4")){
+                                //视频
+                                intent = new Intent(TaskListInfoActivity.this, VideoStreamActivity.class);
+                            }else{
+                                //图片
+                                intent = new Intent(this, PhotoViewerActivity.class);
+                            }
+                            intent.putExtra("url",s);
+                            startActivity(intent);
+                        });
+                        ll_videos.addView(v_);
+                    }
+
+                    binding.llUpload.addView(view);
+                }
+            }
+
+            //消息列表进入-暂时隐藏
+            if(obtainViewModel().message){
+                binding.llButton.setVisibility(View.GONE);
+            }
+        }catch(Exception e){
+//            Toast.makeText(TaskListInfoActivity.this, "数据异常", Toast.LENGTH_SHORT).show();
+            finish();
         }
     }
 

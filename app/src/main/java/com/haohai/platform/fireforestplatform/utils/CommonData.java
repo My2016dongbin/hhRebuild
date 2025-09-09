87f5b9d836e69028866466fb903b46029f7011ad
dongbin <qc.dancer@qq.com>
2025-09-09 11:02:51 +0800

    2.0更换高德地图-贰 -主地图更换

--- app/src/main/java/com/haohai/platform/fireforestplatform/utils/CommonData.java
+++ app/src/main/java/com/haohai/platform/fireforestplatform/utils/CommonData.java
@@ -1,6 +1,5 @@
 package com.haohai.platform.fireforestplatform.utils;
 
-import com.baidu.trace.LBSTraceClient;
 import com.haohai.platform.fireforestplatform.ui.bean.VideoDeleteModel;
 
 import java.util.ArrayList;
@@ -42,9 +41,6 @@ public class CommonData {
     public static String videoDeleteChannelId = "";
     public static List<Integer> videoPlayingIndexList = new ArrayList<>();
     public static List<VideoDeleteModel> videoDeleteModelList = new ArrayList<>();
-
-    //百度鹰眼轨迹
-    public static LBSTraceClient mTraceClient;
 
     public static int mainTabIndex = 0;
     public static String search;

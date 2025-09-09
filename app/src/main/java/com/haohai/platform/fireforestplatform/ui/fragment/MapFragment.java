d5cc46c574cfaa89e95d26e7c4adf2ab00799c17
dongbin <qc.dancer@qq.com>
2025-09-09 17:07:19 +0800

    2.0更换高德地图-贰 -主地图更换-冲突

--- app/src/main/java/com/haohai/platform/fireforestplatform/ui/fragment/MapFragment.java
+++ app/src/main/java/com/haohai/platform/fireforestplatform/ui/fragment/MapFragment.java
@@ -225,7 +225,7 @@ public class MapFragment extends BaseFragment<FgMap, FgMapViewModel> implements 
             @Override
             public void run() {
                 try{
-                    double[] doubles = LatLngChangeNew.calWGS84toBD09(event.getLat(), event.getLng());
+                    double[] doubles = LatLngChangeNew.calWGS84toGCJ02(event.getLat(), event.getLng());
                     flyBaiduMapZoom(doubles[0],doubles[1], 14);
                 }catch (Exception e){
                     Log.e(TAG, "oneBodyFireChanged: " + e.getMessage() );
@@ -464,7 +464,7 @@ public class MapFragment extends BaseFragment<FgMap, FgMapViewModel> implements 
         //跳转第一火点
         try{
             OneBodyFire oneBody = oneBodyFires.get(0);
-            double[] doubles = LatLngChangeNew.calWGS84toBD09(Double.parseDouble(oneBody.getAlarmLatitude()), Double.parseDouble(oneBody.getAlarmLongitude()));
+            double[] doubles = LatLngChangeNew.calWGS84toGCJ02(Double.parseDouble(oneBody.getAlarmLatitude()), Double.parseDouble(oneBody.getAlarmLongitude()));
             flyBaiduMapZoom(doubles[0],doubles[1], 14);
         }catch (Exception e){
             Log.e(TAG, "oneBodyFireChanged: " + e.getMessage() );
@@ -537,7 +537,7 @@ public class MapFragment extends BaseFragment<FgMap, FgMapViewModel> implements 
         //跳转第一火点
         try{
             SatelliteFire satellite = satelliteFires.get(0);
-            double[] doubles = LatLngChangeNew.calWGS84toBD09(Double.parseDouble(satellite.getLatitude()), Double.parseDouble(satellite.getLongitude()));
+            double[] doubles = LatLngChangeNew.calWGS84toGCJ02(Double.parseDouble(satellite.getLatitude()), Double.parseDouble(satellite.getLongitude()));
             flyBaiduMapZoom(doubles[0],doubles[1], 14);
         }catch (Exception e){
             Log.e(TAG, "oneBodyFireChanged: " + e.getMessage() );
@@ -552,7 +552,7 @@ public class MapFragment extends BaseFragment<FgMap, FgMapViewModel> implements 
         if(now - CommonData.longAdding < 5000){
             //新添加了资源
             try{
-                double[] doubles = LatLngChangeNew.calWGS84toBD09(CommonData.latAdding, CommonData.lngAdding);
+                double[] doubles = LatLngChangeNew.calWGS84toGCJ02(CommonData.latAdding, CommonData.lngAdding);
                 flyBaiduMapZoom(doubles[0],doubles[1], 14);
             }catch (Exception e){
                 Log.e(TAG, "resourceChanged: " + e.getMessage() );
@@ -561,7 +561,7 @@ public class MapFragment extends BaseFragment<FgMap, FgMapViewModel> implements 
             //跳转第一个资源点
             try{
                 Resource resource = resources.get(0);
-                double[] doubles = LatLngChangeNew.calWGS84toBD09(Double.parseDouble(resource.getPosition().getLat()), Double.parseDouble(resource.getPosition().getLng()));
+                double[] doubles = LatLngChangeNew.calWGS84toGCJ02(Double.parseDouble(resource.getPosition().getLat()), Double.parseDouble(resource.getPosition().getLng()));
                 flyBaiduMapZoom(doubles[0],doubles[1], 14);
             }catch (Exception e){
                 Log.e(TAG, "resourceChanged: " + e.getMessage() );

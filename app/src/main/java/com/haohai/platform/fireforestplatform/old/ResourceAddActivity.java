b372cc3eff126b149221f91aead35e12b69ef601
dongbin <qc.dancer@qq.com>
2025-09-09 18:55:48 +0800

    2.0更换高德地图-贰 -主地图更换-冲突

--- app/src/main/java/com/haohai/platform/fireforestplatform/old/ResourceAddActivity.java
+++ app/src/main/java/com/haohai/platform/fireforestplatform/old/ResourceAddActivity.java
@@ -1805,7 +1805,7 @@ public class ResourceAddActivity extends BaseActivity implements ChooseImageView
             try {
                 double lat_ = Double.parseDouble(latEdit.getText().toString());
                 double lng_ = Double.parseDouble(lngEdit.getText().toString());
-                double[] doubles = LatLngChangeNew.calBD09toWGS84(lat_, lng_);
+                double[] doubles = LatLngChangeNew.calBD09toGCJ02(lat_, lng_);
                 postsion.put("lat", Double.parseDouble(parseDoubleCount(doubles[0]+"")));
                 postsion.put("lng", Double.parseDouble(parseDoubleCount(doubles[1]+"")));
             } catch (Exception e) {

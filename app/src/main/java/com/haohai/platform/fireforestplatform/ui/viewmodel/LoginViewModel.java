4c1ad43f6452055d75bac5d21207d9a0242ae64b
qc <1349975620@qq.com>
2026-04-24 10:13:34 +0800

    Update tagSet to include new groupId prefix

--- app/src/main/java/com/haohai/platform/fireforestplatform/ui/viewmodel/LoginViewModel.java
+++ app/src/main/java/com/haohai/platform/fireforestplatform/ui/viewmodel/LoginViewModel.java
@@ -153,7 +153,7 @@ public class LoginViewModel extends BaseViewModel {
                                 Set<String> tagSet = new LinkedHashSet<>();
                                 tagSet.add(userJsonObj.getString("id"));
                                 tagSet.add(userJsonObj.getString("gridNo"));
-                                //tagSet.add("wwyt_" + userJsonObj.getString("groupId"));
+                                tagSet.add("etuokeqq_" + userJsonObj.getString("groupId"));
                                 tagSet.add("0a04ec933daf42d6a2");
                                 XGPushManager.setTags(context, "setTag", tagSet);
 

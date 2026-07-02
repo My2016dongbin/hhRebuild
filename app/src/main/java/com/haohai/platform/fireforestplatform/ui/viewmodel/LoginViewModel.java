ef26f53740a555e07ecc1ed10fdbeebf66f2a0f1
dongbin <qc.dancer@qq.com>
2026-07-02 11:28:04 +0800

    推送标签

--- app/src/main/java/com/haohai/platform/fireforestplatform/ui/viewmodel/LoginViewModel.java
+++ app/src/main/java/com/haohai/platform/fireforestplatform/ui/viewmodel/LoginViewModel.java
@@ -156,7 +156,7 @@ public class LoginViewModel extends BaseViewModel {
 
                                 Set<String> tagSet = new LinkedHashSet<>();
                                 tagSet.add(userJsonObj.getString("id"));
-                                tagSet.add(userJsonObj.getString("gridNo"));
+                                tagSet.add("dq_" + userJsonObj.getString("gridNo"));
                                 //tagSet.add(userJsonObj.getString("groupId"));
                                 tagSet.add("dq_" + userJsonObj.getString("groupId"));
                                 //tagSet.add(CommonData.pushFlag);
